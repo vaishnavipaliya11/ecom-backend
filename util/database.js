e52fd643fca3505aa7619ae0815eef39506febee
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2023-09-10 16:37:35 +0530

    db changes

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('e-commerce', 'root', 'abcd', {
+const sequelize = new Sequelize("store_7bcb", 'store_7bcb_user', 'H4m8i5z9N0S43AURCTRBF4i77MVWcSHo', {
   dialect: 'mysql',
-  host: 'localhost'
+  host: 'dpg-cjuq4dnhdsdc73e9m6g0-a'
 });
 
 module.exports = sequelize;
